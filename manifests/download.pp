652cd0f41dace076ecb62c5d70868870de263c04
Thomas Davids <thomas@udacity.com>
2014-12-08 15:42:10 -0800

    WIP

--- manifests/download.pp
+++ manifests/download.pp
@@ -8,30 +8,30 @@ class riemann::download (
   include vcsrepo
   include wget
 
-  case $provider {
-    git, hg, cvs, p4: {
-      vcsrepo { $destination:
-        ensure => present,
-        provider => $provider,
-        source => $source,
-        revision => $revision,
-        before => Exec['untar_riemann'],
-      }
-    }
-    default: {
-      wget::fetch { 'download_riemann':
-        source      => $source,
-        destination => $destination,
-        before      => Exec['untar_riemann'],
-      }
-    }
-  }
+  # case $provider {
+  #   git, hg, cvs, p4: {
+  #     vcsrepo { $destination:
+  #       ensure => present,
+  #       provider => $provider,
+  #       source => $source,
+  #       revision => $revision,
+  #       before => Exec['untar_riemann'],
+  #     }
+  #   }
+  #   default: {
+  #     wget::fetch { 'download_riemann':
+  #       source      => $source,
+  #       destination => $destination,
+  #       before      => Exec['untar_riemann'],
+  #     }
+  #   }
+  # }
 
-  exec { 'untar_riemann':
-    command => "/bin/tar --bzip2 -xvf /usr/local/src/riemann-${riemann::version}.tar.bz2",
-    cwd     => '/opt',
-    creates => "/opt/riemann-${riemann::version}/bin/riemann",
-    user    => $user,
-    require => File["/opt/riemann-${riemann::version}"],
-  }
+  # exec { 'untar_riemann':
+  #   command => "/bin/tar --bzip2 -xvf /usr/local/src/riemann-${riemann::version}.tar.bz2",
+  #   cwd     => '/opt',
+  #   creates => "/opt/riemann-${riemann::version}/bin/riemann",
+  #   user    => $user,
+  #   require => File["/opt/riemann-${riemann::version}"],
+  # }
 }
